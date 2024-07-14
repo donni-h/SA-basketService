3179ab49133c0b85f8cc5a3ab09e79670877dee5
Marcibouz <marcio_holz@gmx.de>
2024-07-14 19:10:57 +0200

    finished model and changed pom.xml

--- src/main/java/de/htw/SA_basketService/core/domain/model/Basket.java
+++ src/main/java/de/htw/SA_basketService/core/domain/model/Basket.java
@@ -1,4 +1,33 @@
 package de.htw.SA_basketService.core.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Entity //Marks class as database Entity
+@Table  //Table in Data Base (here you can name the table)
+@Getter
+@Setter
+@NoArgsConstructor
 public class Basket {
+    @Id
+    @NotNull(message = "userId cannot be null")
+    private UUID userId;
+
+    @NotNull(message = "Items cannot be null")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "userId")
+    private List<Item> items;
+
+    @NotNull (message = "totalPrice cannot be null")
+    @PositiveOrZero(message = "totalPrice has to be positive or zero")
+    private BigDecimal totalPrice;
 }
--- src/main/java/de/htw/SA_basketService/core/domain/model/Item.java
+++ src/main/java/de/htw/SA_basketService/core/domain/model/Item.java
@@ -1,4 +1,57 @@
 package de.htw.SA_basketService.core.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity //Marks class as database Entity
+@Table  //Table in Data Base (here you can name the table)
+@Getter
+@Setter
+@NoArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID itemId;
+
+    @NotNull(message = "name cannot be null")
+    @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
+    private String name;
+
+    @NotNull (message = "item Price cannot be null")
+    @PositiveOrZero(message = "item Price has to be positive or zero")
+    private BigDecimal itemPrice;
+
+    @NotNull (message = "Image link cannot be null")
+    private String imageLink;
+
+    public Item(UUID itemId, String name, BigDecimal itemPrice, String imageLink) {
+        this.itemId = itemId;
+        this.name = name;
+        this.itemPrice = itemPrice;
+        this.imageLink = imageLink;
+    }
+
+    public Item(String name, BigDecimal itemPrice, String imageLink) {
+        this.name = name;
+        this.itemPrice = itemPrice;
+        this.imageLink = imageLink;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemId=" + itemId +
+                ", name='" + name + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", imageLink='" + imageLink + '\'' +
+                '}';
+    }
 }
